9c9f4cf00975fd259d2ca14c1e48000fbdebdf15
Jesper <cph-jr211@cphbusiness.dk>
2019-05-14 21:41:29 +0200

    fix

--- helpmappe/htmlPages.js
+++ helpmappe/htmlPages.js
@@ -1,11 +1,12 @@
 //Initial setup
+//Når siden loader bliver siden sat til hjemme siden
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('page').innerHTML = home
 });
 
-//Præcis kopi af vores front side, det er så vi kan komme tilbage til den :)
+//Front page
 let home = `
-<h1>Husk at give jeres body en ID</h1>
+<h1 id="hej">Husk at give jeres body en ID</h1>
 <h2> her er hoved siden </h2>
 <h3>Men jeg har også en kopi af denne side der hedder "html1" i min javascript :))</h3>
 <p> her er en masse billeder</p>
@@ -46,16 +47,28 @@ let carPage = `
 <button id="iceCreamPage"> Is-Siden! </button>
 <button id="home"> Home </button>
 `
-
+//Her sker alt magien, vi kigger på om en knap bliver trykket på
+//Hvis den gør så sker følgende
 document.getElementById('page').addEventListener('click', function (e) {
+    // Vi har vores pages liggende i et objekt så vi let kan hente den side vi ønsker
+    // Hvis vi vil tilføje en ny side, er det eneste vi nu skal gøre at
+    //Tilføje den til pages objektet og selvfølgelig have en knap der linker dertil
+    // Men så er alt det andet faktisk automatisk :)
     let pages = {
         carPage: carPage,
         iceCreamPage, iceCreamPage,
         home: home
     }
+    //Her ser vi om det er et id der er trykket på
     if (e.target.id) {
+        //Hvis det er, så laver vi HTML'en om til den knap der er trykket på
+        //Vi forsøger kun at lave siden om hvis at id'et findes i objektet "pages"
         let id = e.target.id
+        if(pages[id] != undefined){
+            console.log("id found")
+        
         document.getElementById('page').innerHTML = pages[id]
+    }
     }
 })
 
